6ce92b7950c119928276bdf1122e41d94cb9be0c
Karthik Prabhu Nagaraja (BD/SWD-DOE2) <Nagaraja.KarthikPrabhu@in.bosch.com>
2025-06-30 11:05:52 +0530

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # spring-boot-parent-repo
-Multi model project to hands on spring boot modules
+Multi-model project to hands on spring boot modules
+Spring Boot to create a re-usable library and then used that library to build an application.
---
+++ library/src/test/java/com/example/multimodule/service/MyServiceTest.java
@@ -0,0 +1,26 @@
+package com.example.multimodule.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest("service.message=Hello from MyServiceTest")
+public class MyServiceTest {
+
+    @Autowired
+    private MyService myService;
+
+    @Test
+    public void contextLoads() {
+        assertThat(myService.message()).isNotNull();
+    }
+
+
+    @SpringBootApplication
+    static class TestConfiguration {
+    }
+
+}
